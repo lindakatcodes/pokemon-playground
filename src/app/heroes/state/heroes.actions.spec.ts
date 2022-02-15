a765648ad18cc53bb3b3ba9700909da9477d0754
LindaKat <lindakt16@gmail.com>
2022-02-14 23:21:31 -0600

    removed unused tests; removed f from pokemon test

--- src/app/heroes/state/heroes.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as fromHeroes from './heroes.actions';
-
-describe('loadHeroes', () => {
-  it('should return an action', () => {
-    expect(fromHeroes.loadHeroes().type).toBe('[Heroes] Load Heroes');
-  });
-});
